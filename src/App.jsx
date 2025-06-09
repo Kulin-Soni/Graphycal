8e1ed78819d3a5e1da823ef3c87b9afb1aa45944
Kulin Soni <grozersniper38237@gmail.com>
2025-06-09 17:14:17 +0530

    Project Cleaned
    A new and clean project will be built that accomodates all the screens, the files will be in the next commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import React from "react";
-import "./Theme.css";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Graph from "./components/Graph";
-import Controls from "./components/Controls";
-function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <div className="body">
-        <div className="content">
-          <div>
-            <Graph />
-          </div>
-          <div>
-            <Controls />
-          </div>
-        </div>
-      </div>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
